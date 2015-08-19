ebd4d13326d2011be36037b440343ceb7fe5e4f5
GCA Savvian <gcasawian@gmail.com>
2015-08-20 07:36:44 +0900

    auto commit

--- make_index.sh
+++ make_index.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ls g*_c*_a*.html > ls.txt
-sed -E 's|(.*)|<a href="\1">GCA: \1</a><br/>|g' ls.txt > index.txt
+sed -E 's|(.*)\.html|<a href="\1.html">GCA: \1</a><br/>|g' ls.txt > index.txt
 
 echo "<html><head><title>GCA Savvian</title></head><body>" > index.html
+echo "<div>Abbreviations for GCA</div>" >> index.html
 cat index.txt >> index.html
 echo "<a href=\"http://www.gcasavvian.com/jp/\">GCA Savvian Corporation</a>" >> index.html
 echo "</body></html>" >> index.html
